ebec36a418e5bb50c095b283c266558a238ae97f
Filippo Toso <filippo.toso@creativepark.it>
2018-06-15 11:32:27 +0200

    Improve support for business account

--- Client.php
+++ Client.php
@@ -17,13 +17,19 @@ class Client
     protected $cookieJar;
 
     /**
+     * Change the urls if the user is logged in
+     * @var boolean
+     */
+    public $loggedIn = false;
+
+    /**
      * Define the language of the interface
      * @var string
      */
     protected $lang = 'en-US';
 
     protected function newClient() {
-        return  new HTTPClient([
+        return new HTTPClient([
             'cookies' => true,
             'headers' => [
                 'Accept-Language' => $this->lang,
@@ -32,7 +38,7 @@ class Client
     }
 
     /**
-     * Execute an HTTP GET request to Qwant API
+     * Execute an HTTP GET request
      * @param  String $url The url of the API endpoint
      * @return Array|FALSE  The result of the request
      */
@@ -50,6 +56,7 @@ class Client
             ]);
         }
         catch (BadResponseException $e) {
+            dd( $e);
             return FALSE;
         }
 
@@ -60,7 +67,7 @@ class Client
     }
 
     /**
-     * Execute an HTTP GET request to Qwant API
+     * Execute an HTTP GET request
      * @param  String $url The url of the API endpoint
      * @return Array|FALSE  The result of the request
      */
@@ -86,7 +93,7 @@ class Client
 
 
     /**
-     * Execute an HTTP POST request to Qwant API
+     * Execute an HTTP POST request
      * @param  String $url The url of the API endpoint
      * @param  Array $data The parameters of the request
      * @return Array|FALSE  The result of the request
@@ -116,7 +123,7 @@ class Client
     }
 
     /**
-     * Execute an HTTP POST request to Qwant API
+     * Execute an HTTP POST request
      * @param  String $url The url of the API endpoint
      * @param  Array $data The parameters of the request
      * @return Array|FALSE  The result of the request
@@ -143,13 +150,43 @@ class Client
     }
 
     /**
+     * Execute an HTTP request
+     * @param  String $url The url of the API endpoint
+     * @param  Array $data The parameters of the request
+     * @return Array|FALSE  The result of the request
+     */
+    protected function json($url, $data = [], $method = 'POST') {
+
+        $client = $this->newClient();
+
+        try {
+            $res = $client->request($method, $url, [
+                'json' => $data,
+                'headers' => [
+                    'Accept'     => 'application/json',
+                    'User-Agent' => 'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0',
+                ],
+                'cookies' => $this->cookieJar,
+            ]);
+        }
+        catch (BadResponseException $e) {
+            return FALSE;
+        }
+
+        $data = json_decode($res->getBody(), TRUE);
+
+        return $data;
+
+    }
+
+    /**
      * Generate an API url based on the provided path
      * @method getUrl
      * @param  String $path The api call path
      * @return String      The result API url
      */
     protected function getUrl($path, $api = true) {
-        $baseUrl = $api ? 'https://www.lefrecce.it/msite/api' : 'https://www.lefrecce.it/msite';
+        $baseUrl = $api ? 'https://www.lefrecce.it/msite/api/users' : 'https://www.lefrecce.it/msite';
         return sprintf('%s/%s', $baseUrl, $path);
     }
 
@@ -166,7 +203,7 @@ class Client
     /**
      * Autocomplete locations names
      * @method locations
-     * @param  string    $name
+     * @param  String    $name
      * @return array
      */
     public function locations($name) {
@@ -179,12 +216,12 @@ class Client
      * @method solutions
      * @param  [type]    $origin       [description]
      * @param  [type]    $destination  [description]
-     * @param  string    $arflag       [description]
+     * @param  String    $arflag       [description]
      * @param  [type]    $adate        [description]
      * @param  [type]    $atime        [description]
      * @param  integer   $adultno      [description]
      * @param  integer   $childno      [description]
-     * @param  string    $direction    [description]
+     * @param  String    $direction    [description]
      * @param  boolean   $frecce       [description]
      * @param  boolean   $onlyRegional [description]
      * @param  [type]    $rdate        [description]
@@ -207,15 +244,22 @@ class Client
             'direction' => in_array($direction, ['A', 'R']) ? $direction : 'A',
             'frecce' => $frecce ? 'true' : 'false',
             'onlyRegional' => $onlyRegional ? 'true' : 'false',
-            'rdate' => is_null($rdate) ? date('d/m/Y', time() + 60 * 60 * 24) : $rdate,
-            'rtime' => is_null($rtime) ? date('H') : $rtime,
         ];
 
         if (!is_null($codeList)) {
             $params['codeList'] = $codeList;
         }
 
+        if (!is_null($rdate)) {
+            $params['rdate'] = $rdate;
+        }
+
+        if (!is_null($rtime)) {
+            $params['rtime'] = $rtime;
+        }
+
         $url = $this->getUrl('solutions?' . http_build_query($params));
+
         return $this->getJSON($url);
 
     }
@@ -223,7 +267,7 @@ class Client
     /**
      * Get solution standard details
      * @method solutionDetails
-     * @param  string $solutionId The solution id
+     * @param  String $solutionId The solution id
      * @return array
      */
     public function solutionDetails($solutionId) {
@@ -234,7 +278,7 @@ class Client
     /**
      * Get solution standard info
      * @method solutionInfo
-     * @param  string $solutionId The solution id
+     * @param  String $solutionId The solution id
      * @return array
      */
     public function solutionInfo($solutionId) {
@@ -245,7 +289,7 @@ class Client
     /**
      * Get solution standard offers
      * @method solutionStandardOffers
-     * @param  string $solutionId The solution id
+     * @param  String $solutionId The solution id
      * @return array
      */
     public function solutionStandardOffers($solutionId) {
@@ -256,7 +300,7 @@ class Client
     /**
      * Get solution customized offers
      * @method solutionCustomizedOffers
-     * @param  string $solutionId The solution id
+     * @param  String $solutionId The solution id
      * @return array
      */
     public function solutionCustomizedOffers($solutionId) {
@@ -267,8 +311,8 @@ class Client
     /**
      * Logs in
      * @method login
-     * @param  string $username The user's username
-     * @param  string $password The user's password
+     * @param  String $username The user's username
+     * @param  String $password The user's password
      * @param  boolean $forceUpperCase By default the website requires uppercase password
      * @return string
      */
@@ -279,7 +323,8 @@ class Client
             'j_password' => $forceUpperCase ? strtoupper($password) : $password,
         ];
 
-        $url = $this->getUrl('users/login');
+        $url = $this->getUrl('login');
+
         return $this->post($url, $params);
 
     }
@@ -300,7 +345,7 @@ class Client
      * @return array
      */
     public function userProfile() {
-        $url = $this->getUrl('users/profile');
+        $url = $this->getUrl('profile');
         return $this->getJSON($url);
     }
 
@@ -321,7 +366,7 @@ class Client
             'searchbydeparture' => $searchbydeparture ? 'true' : 'false',
         ];
 
-        $url = $this->getUrl('users/purchases?' . http_build_query($params));
+        $url = $this->getUrl('purchases?' . http_build_query($params));
 
         return $this->getJSON($url);
 
@@ -330,25 +375,25 @@ class Client
     /**
      * Get the details of a specific purchase / sale
      * @method saleDetails
-     * @param  string      $saleId The sale Id
+     * @param  String      $saleId The sale Id
      * @return array
      */
     public function saleDetails($saleId) {
-        $url = $this->getUrl(sprintf('users/sales/%s', urlencode($saleId)));
+        $url = $this->getUrl(sprintf('sales/%s', urlencode($saleId)));
         return $this->getJSON($url);
     }
 
     /**
      * Download a ticket associated with a specific purchase / sale
      * @method downloadTicket
-     * @param  string      $saleId    The sale Id
+     * @param  String      $saleId    The sale Id
      * @param  integer     $tsId      The ticket Id. For single purchases it will be 1 (the default value)
-     * @param  string|null $filename  The path where to save the ticket. If null, the function returns the file content.
+     * @param  String|null $filename  The path where to save the ticket. If null, the function returns the file content.
      * @return string
      */
     public function downloadTicket($saleId, $tsId = 1, $filename = null) {
 
-        $url = $this->getUrl(sprintf('users/sales/%s/travel?lang=it&tsid=%s', urlencode($saleId), urlencode($tsId)));
+        $url = $this->getUrl(sprintf('sales/%s/travel?lang=it&tsid=%s', urlencode($saleId), urlencode($tsId)));
 
         $content = $this->get($url);
 
@@ -372,6 +417,146 @@ class Client
             $this->lang = $lang;
         }
         return $this->lang;
+    }
+
+    /**
+     * [travels description]
+     * @method travels
+     * @param  String  $solutionId The current solution Id
+     * @param  Array  $selections An assoc array with one or more xmlid and travelertype
+     * @return Array
+     */
+    public function travels($solutionId, $selections) {
+
+        $url = $this->getUrl('travels');
+        $params = [
+            'idsolution' => $solutionId,
+            'selections' => $selections,
+            'revalidate' => true,
+        ];
+
+        return $this->postJSON($url, $params);
+
+    }
+
+    /**
+     * [sals description]
+     * @method sales
+     * @param  Array $travelIds The current travel Ids
+     * @return Array
+     */
+
+    public function sales($travelIds) {
+
+        $url = $this->getUrl('sales');
+
+        $params = [];
+        foreach ($travelIds as $travelId) {
+            $params[] = ['idtravel' => $travelId];
+        }
+
+        return $this->postJSON($url, $params);
+
+    }
+
+    /**
+     * [salesTravelers description]
+     * @method salesTravelers
+     * @param  String         $travelId The current travel Id
+     * @param  String         $offerId  The current offer Id
+     * @return Array
+     */
+    public function salesTravelers($travelId, $offerId) {
+        $url = $this->getUrl(sprintf('sales/%s/travellers/details?offeredservicelist=%s', urlencode($travelId), urlencode($offerId)));
+        return $this->getJSON($url);
+    }
+
+    /**
+     * [salesPassengers description]
+     * @method salesPassengers
+     * @param  String          $travelId  [description]
+     * @param  String          $arflag    [description]
+     * @param  Array          $travelers [description]
+     * @return Array
+     */
+    public function salesPassengers($travelId, $arflag, $travelers) {
+
+        $url = $this->getUrl(sprintf('sales/%s/passengers', urlencode($travelId)));
+
+        $params = [
+            'arflag' => $arflag,
+            'validate' => true,
+        ];
+
+        $defaults = [
+            'Tipo Viaggiatore' => 'ADULTO',
+            'Nome' => '',
+            'Cognome' => '',
+            'Loyalty Code' => '',
+            'Data di nascita' => '',
+            'Tipo documento' => '',
+            'Numero documento' => '',
+            'DATA_DOCUMENTO' => '',
+            'Nazione' => '',
+            'Provincia di emissione' => '',
+            'Comune di emissione' => '',
+            'NAZIONE_DI_NASCITA' => '',
+            'PROVINCIA_DI_NASCITA' => '',
+            'COMUNE_DI_NASCITA' => '',
+        ];
+
+        foreach ($travelers as $id => $travelerParams) {
+
+            $traveler = [
+                'id' => $id,
+            ];
+
+            $travelerParams = array_merge($defaults, $travelerParams);
+
+            foreach ($travelerParams as $key => $value) {
+                $traveler['travellerParameters'][] = [
+                    'name' => $key,
+                    'value' => $value,
+                ];
+            }
+
+            $params['travelers'][] = $traveler;
+        }
+
+        return $this->json($url, $params, 'PUT');
+
+    }
+
+    /**
+     * [salesPaymentModes description]
+     * @method salesPaymentModes
+     * @return [type]            [description]
+     */
+    public function salesPaymentModes($travelId) {
+
+        $url = $this->getUrl(sprintf('sales/%s/paymentmodes?isPostoClick=false&isInvoice=undefined', urlencode($travelId)));
+        return $this->getJSON($url);
+
+    }
+
+    public function salesOrder($travelId, $paymentId, $amount, $invoice = false) {
+
+        $url = $this->getUrl(sprintf('sales/%s/order', urlencode($travelId)));
+
+        $params = [
+            'invoice' => $invoice,
+            'orderParameterList' => null,
+            'pin' => '',
+            'payments' => [
+                [
+                    'paymentid' => $paymentId,
+                    'amount' => number_format($amount, 2),
+                ]
+            ],
+        ];
+
+        return $this->postJSON($url, $params);
+
     }
 
 }
